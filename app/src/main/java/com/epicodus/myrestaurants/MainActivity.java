aa783fede0ebc2a38230b872bb8b8115f30fb234
Shelby Clayton <shelby_clayton@hotmail.com>
2016-11-13 22:33:29 -0800

    successfully pass zip code between main and restaurant activity

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -7,10 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
 
@@ -19,15 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button)
-                findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(
-        new View.OnClickListener() {
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
